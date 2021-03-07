534cafb2040cc6ab6c0fcc024871b636cf9ba1fe
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-03-07 20:35:49 +0530

    user: Add mongoose user model.
    Define user schema using mongoose to validate user documents are store correctly in mongodb. It exports User model to perfrom CURD operations related to user.

---
+++ src/models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: mongoose.Types.ObjectId,
+    username: {
+      type: String,
+      unique: true,
+    },
+    displayName: {
+      type: String,
+      default: "Anonymous",
+    },
+    emailId: {
+      type: String,
+      required: true,
+    },
+    interests: [String],
+    connections: {
+      type: [String],
+      ref: "User",
+    },
+    refreshToken: {
+      type: String,
+      unique: true,
+    },
+    summary: {
+      type: String,
+    },
+    publishedCourses: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    recommended: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    recommendations: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    isComplete: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { _id: false, timestamps: true }
+);
+
+userSchema.virtual("reviews", {
+  ref: "Review",
+  localField: "_id",
+  foreignField: "userId",
+});
+
+module.exports = mongoose.model("User", userSchema);
