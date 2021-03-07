a8d1c47e02d70bbcc0878ad1bde45873fe34dfe0
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-03-07 20:45:31 +0530

    auth: Add OAuth for user authentication.
    Add passport middleware with Google OAuth startergy and auth routes to authenticate users & signin/signup into platform.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const passport = require("passport");
+const connect = require("./db/mongoose");
+const config = require("./config/config");
+const authRouter = require("./routes/auth");
+const ErrorHandler = require("./helpers/ErrorHandler");
+const UserService = require("./services/UserService");
+require("./config/passport");
+
+const PORT = config.server.port;
+connect();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(passport.initialize());
 
-app.get("/", (req, res) => {
-  res.send("Hello World Using docker-compose");
+app.use("/auth/", authRouter);
+
+app.use((err, req, res, next) => {
+  ErrorHandler.handleError(err, res);
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
