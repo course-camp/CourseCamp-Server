6c39546bc5688e4ed0b15899ba2f082cb73d5b09
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-03-07 18:51:50 +0530

    service: Add JWTService
    Add JWTservice to perform tasks related to jwt which includes access & refresh token signingwith payload and token verification.

---
+++ src/services/JWTService.js
@@ -0,0 +1,85 @@
+const jwt = require("jsonwebtoken");
+const ObjectId = require("mongoose").Types.ObjectId;
+const {
+  accessSecret,
+  refreshSecret,
+  accessTokenLife,
+  refreshTokenLife,
+} = require("../config/config").jwt;
+const { HTTP401Error, HTTP500Error } = require("../helpers/error");
+class JWTService {
+  static isValidPayload(payload) {
+    if (
+      payload &&
+      typeof payload === "object" &&
+      payload.hasOwnProperty("userId") &&
+      ObjectId.isValid(payload.userId) &&
+      String(new ObjectId(payload.userId)) === String(payload.userId)
+    ) {
+      return true;
+    }
+    return false;
+  }
+  static signAccessToken(payload) {
+    try {
+      if (this.isValidPayload(payload)) {
+        return jwt.sign(payload, accessSecret, { expiresIn: accessTokenLife });
+      }
+      throw new Error("Not valid payload");
+    } catch (error) {
+      throw new HTTP500Error(error.message);
+    }
+  }
+  static signRefreshToken(payload) {
+    try {
+      if (this.isValidPayload(payload)) {
+        return jwt.sign(payload, refreshSecret, {
+          expiresIn: refreshTokenLife,
+        });
+      }
+      throw new APPError("Not valid payload.");
+    } catch (error) {
+      throw new HTTP500Error(error.message);
+    }
+  }
+  static verifyBearerToken(bearerToken) {
+    try {
+      const token = bearerToken.replace("Bearer ", "");
+      if (token) {
+        return jwt.verify(token, accessSecret);
+      }
+      throw new Error("Not valid bearer token.");
+    } catch (error) {
+      throw new HTTP401Error(error.message);
+    }
+  }
+  static verifyRefreshToken(refreshToken) {
+    try {
+      if (refreshToken) {
+        return jwt.verify(refreshToken, refreshSecret);
+      }
+      throw new Error("Not valid refresh token.");
+    } catch (error) {
+      throw new HTTP401Error(error.message);
+    }
+  }
+  static generateTokens(payload) {
+    try {
+      if (this.isValidPayload(payload)) {
+        return {
+          accessToken: jwt.sign(payload, accessSecret, {
+            expiresIn: accessTokenLife,
+          }),
+          refreshToken: jwt.sign(payload, refreshSecret, {
+            expiresIn: refreshTokenLife,
+          }),
+        };
+      }
+      throw new Error("Not valid payload.");
+    } catch (error) {
+      throw new HTTP500Error(error.message);
+    }
+  }
+}
+
+module.exports = JWTService;
