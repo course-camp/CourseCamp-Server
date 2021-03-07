4e4c18131cca0c7b43f4bbed34e0955f8a2302ac
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-03-07 16:10:58 +0530

    error_response: Create seperate file to add error response status & statusCode constants
    Seperate out error response status & statuscode constants to be used and import where need for having constant value in overall code.

---
+++ src/config/error.js
@@ -0,0 +1,17 @@
+const statusCode = {
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+const status = {
+  BAD_REQUEST: "BAD_REQUEST",
+  UNAUTHORIZED: "UNAUTHORIZED",
+  FORBIDDEN: "FORBIDDEN",
+  NOT_FOUND: "NOT_FOUND",
+  INTERNAL_SERVER_ERROR: "INTERNAL_SERVER_ERROR",
+};
+
+module.exports = { statusCode, status };
