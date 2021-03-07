e626b01ee2a189d87b9abaf8a1f1a22b4627d07f
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-03-07 19:07:15 +0530

    helper: Add centralized error handler
    Create centralized error handling mechanism for error related to all endpoints, express middlewares, db or programmer errors.

---
+++ src/helpers/ErrorHandler.js
@@ -0,0 +1,27 @@
+const { APPError, HTTP500Error } = require("./error");
+const mongoose = require("mongoose");
+class ErrorHandler {
+  static handleError(err, res) {
+    if (err instanceof mongoose.Error) {
+      err = new HTTP500Error();
+      delete err.message;
+    }
+    if (err instanceof APPError && err.isOperational) {
+      delete err.isOperational;
+      delete err.name;
+      delete err.stack;
+      if (err.statusCode >= 500) {
+        // TODO: log error and send error events.
+        delete err.message;
+      }
+
+      return res.status(err.statusCode).send(err);
+    }
+
+    //TODO: log error and send error events.
+    //TODO: exit process
+    console.log(err);
+  }
+}
+
+module.exports = ErrorHandler;
