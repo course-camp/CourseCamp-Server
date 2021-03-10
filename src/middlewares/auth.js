539595a28872addb6afcbf4ae655ca80d57af62f
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-03-10 16:43:30 +0530

    auth: fix auth middlware.
    Async getUserById function defined inside of UserService class was called without correctly handling return promise. Also corrected to location to store user profile as it was stored in 'res' instead of 'req'.

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,12 @@
 const JWTService = require("../services/JWTService");
 const UserService = require("../services/UserService");
 
-const auth = function (req, res, next) {
+const auth = async function (req, res, next) {
   try {
     const bearerToken = req.headers["authorization"];
     let user = JWTService.verifyBearerToken(bearerToken);
-    user = UserService.getUserById(user.userId);
-    res.user = user;
+    user = await UserService.getUserById(user.userId);
+    req.user = user;
     next();
   } catch (error) {
     next(error);
