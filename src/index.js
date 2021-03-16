b4e63bf87963322f9ce1591bb89c55d3b7c67594
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-03-16 11:44:08 +0530

    middleware: Move express error handler middleware to src/middlewares.
    Move express error handler middleware logic to src/middlewares in error.js file to make code more modular and
    seperate error handling logic from index.js file.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const passport = require("passport");
 const connect = require("./db/mongoose");
-const config = require("./config/config");
-const ErrorHandler = require("./helpers/ErrorHandler");
+const server = require("./config/config").server;
+const error = require("./middlewares/error");
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const courseRouter = require("./routes/course");
 const reviewRouter = require("./routes/review");
 require("./config/passport");
 
-const PORT = config.server.port;
+const PORT = server.port;
 connect();
 
 const app = express();
@@ -21,8 +21,6 @@ app.use("/users/", userRouter);
 app.use("/courses/", courseRouter);
 app.use("/reviews/", reviewRouter);
 
-app.use((err, req, res, next) => {
-  ErrorHandler.handleError(err, res);
-});
+app.use(error);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
