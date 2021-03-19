9d735919863c96bc0afb5cc7ea0e45f73ebcdc7e
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-03-19 16:55:15 +0530

    controller: Add ADD/DELETE/GET recommendation routes controller function.
    Add controllers functions for routes related to recommendation which includes to ADD/DELETE/GET
    recommendation for a particular course.

--- src/controllers/CourseController.js
+++ src/controllers/CourseController.js
@@ -2,7 +2,13 @@ const CourseService = require("../services/CourseService");
 const validator = require("../helpers/validator");
 const { validateRequestQuery } = require("../helpers/validateRequestQuery");
 const { HTTP400Error } = require("../helpers/error");
-const { HTTP200Success, HTTP201Success } = require("../helpers/success");
+const {
+  HTTP200Success,
+  HTTP201Success,
+  HTTP204Success,
+} = require("../helpers/success");
+const RecommendService = require("../services/RecommendService");
+const ReviewService = require("../services/ReviewService");
 
 class CourseController {
   static async getAllCourses(req, res, next) {
@@ -42,6 +48,8 @@ class CourseController {
       const validate = validator.validateObjectId.validate(courseId);
       if (!validate.error) {
         const course = await CourseService.deleteCourseById(courseId);
+        await RecommendService.deleteRecommendationToCourse(courseId);
+        await ReviewService.deleteReviewToCourse(courseId);
         return new HTTP200Success("Course deleted", { course }).sendResponse(
           res
         );
@@ -59,6 +67,7 @@ class CourseController {
       if (!validate.error) {
         validate.value["publisher"] = user._id;
         const newCourse = await CourseService.addCourse(validate.value);
+        await RecommendService.addRecommendation(newCourse._id, user._id);
         return new HTTP201Success("Course added.", {
           course: newCourse,
         }).sendResponse(res);
@@ -118,6 +127,68 @@ class CourseController {
       }
       throw new HTTP400Error("Invalid courseId.");
       // new HTTP200Success("Course reviews.", { courses }).sendResponse(res);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async addRecommendation(req, res, next) {
+    try {
+      const {
+        params: { courseId },
+        user,
+      } = req;
+      let { value, error } = validator.validateObjectId.validate(courseId);
+      if (!error) {
+        const course = await CourseService.getCourseById(value);
+        await RecommendService.addRecommendation(course._id, user._id);
+        await CourseService.updateCourseRecommendCount(value, true, course);
+        return new HTTP204Success("Recommendation added").sendResponse(res);
+      }
+      throw new HTTP400Error("Invalid courseId.");
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async deleteRecommendation(req, res, next) {
+    try {
+      const {
+        params: { courseId },
+        user,
+      } = req;
+      let { value, error } = validator.validateObjectId.validate(courseId);
+      if (!error) {
+        await RecommendService.deleteRecommendation(value, user._id);
+        await CourseService.updateCourseRecommendCount(value, false);
+        return new HTTP204Success("Recommendation deleted.").sendResponse(res);
+      }
+      throw new HTTP400Error("Invalid courseId.");
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getRecommendedBy(req, res, next) {
+    try {
+      const {
+        params: { courseId },
+        user,
+      } = req;
+      let { value, error } = validator.validateObjectId.validate(courseId);
+      if (!error) {
+        const options = await validateRequestQuery(req.query);
+        const course = await CourseService.getCourseById(value);
+        const recommendedBy = await RecommendService.getRecommendedBy(
+          course._id,
+          user._id,
+          options
+        );
+        return new HTTP200Success("List of users recommending course.", {
+          recommendedBy,
+        }).sendResponse(res);
+      }
+      throw new HTTP400Error("Invalid courseId.");
     } catch (error) {
       next(error);
     }
