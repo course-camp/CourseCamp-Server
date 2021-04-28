73d12ef3ab9884e4e83e8d9d10b05195cfb0982e
Sudhir-Pawar <pawarsudhir8498@gmail.com>
2021-04-28 20:21:47 +0530

    helpers: Update Errorhandler to catch Axios error.
    Update Errorhandler to catch errors thrown by axios module. In cases when Axios throws an error
    then server must respond with a HTTP500 error as its internal server error which might be caused
    by such as if file server is down.

--- src/helpers/ErrorHandler.js
+++ src/helpers/ErrorHandler.js
@@ -1,10 +1,17 @@
-const { HTTPError, HTTP500Error } = require("./error");
+const { HTTPError, HTTP500Error, HTTP400Error } = require("./error");
 const mongoose = require("mongoose");
+const { MulterError } = require("multer");
+const axios = require("axios").default;
+
 class ErrorHandler {
   static handleError(err, res) {
-    if (err instanceof mongoose.Error) {
+    if (err instanceof mongoose.Error || axios.isAxiosError(err)) {
+      //TODO: log errors
       err = new HTTP500Error();
       delete err.message;
+    }
+    if (err instanceof MulterError) {
+      err = new HTTP400Error("Bad request paramters.");
     }
     if (err instanceof HTTPError && err.isOperational) {
       delete err.isOperational;
@@ -14,6 +21,7 @@ class ErrorHandler {
         // TODO: log error and send error events.
         delete err.message;
       }
+      if (err.hasOwnProperty("storageErrors")) delete err.storageErrors;
 
       return res.status(err.statusCode).send(err);
     }
